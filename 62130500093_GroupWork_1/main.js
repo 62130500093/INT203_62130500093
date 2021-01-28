5813199a4d1f53a94643ea62d52e1297183668af
Sahachai Senarak <sahachai.kmutt@mail.kmutt.ac.th>
2021-01-29 01:36:41 +0700

    [update] some js and search form

--- 62130500093_GroupWork_1/main.js
+++ 62130500093_GroupWork_1/main.js
@@ -5,4 +5,19 @@ const app = {
         }
     }
 }
-Vue.createApp(app).mount('#app')
+Vue.createApp(app).mount('#app');
+
+let search = document.getElementById("search-container-sub");
+
+$(document).ready(function () {
+    $("#search-element-2-input").on("input", function () {
+        if ($(this).val() == "") {
+            $("#search-element-3").hide();
+            search.style.borderRight = "0";
+        }
+        else {
+            $("#search-element-3").show();
+            search.style.borderRight = "1px solid #dfe1e5";
+        }
+    });
+});
