d6aad2907bbf8ec45de20cff0336d0975501cb05
Sahachai Senarak <sahachai.kmutt@mail.kmutt.ac.th>
2021-01-29 23:42:50 +0700

    [add] js for search form and submit page and a bit detail (complete / nearly)

--- 62130500093_GroupWork_1/main.js
+++ 62130500093_GroupWork_1/main.js
@@ -1,14 +1,17 @@
-const app = {
-    data() {
-        return {
-            msg: 'Hello, Vue3'
+var message = localStorage.getItem("msg");
+
+if (message != null) {
+    const app = {
+        data() {
+            return {
+                msg: message
+            }
         }
     }
+    Vue.createApp(app).mount('#app');
 }
-Vue.createApp(app).mount('#app');
 
 let search = document.getElementById("search-container-sub");
-
 $(document).ready(function () {
     $("#search-element-2-input").on("input", function () {
         if ($(this).val() == "") {
@@ -18,6 +21,22 @@ $(document).ready(function () {
         else {
             $("#search-element-3").show();
             search.style.borderRight = "1px solid #dfe1e5";
+            $("#search-element-3").on("click", function () {
+                $("#search-element-2-input").val("");
+                $("#search-element-3").hide();
+                search.style.borderRight = "0";
+            });
         }
     });
 });
+function searchForm() {
+    if ($("#search-element-2-input").val() == "") { }
+    else {
+        $("#search-element-2").submit();
+        localStorage.setItem("msg", $("#search-element-2-input").val());
+    }
+};
+function toHome() {
+    localStorage.setItem("msg", null);
+    window.open("./index.html", "_self");
+}
